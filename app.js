b0595e16a6f8d0d20f8bf25970f883305ab901eb
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2021-06-03 00:06:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,59 +9,59 @@ const localTime = document.getElementById('current-time');
 
 
 
-//submit.addEventListener('click', () => {
-//
-//    const proxy = 'http://cors-anywhere.herokuapp.com/';
-//    const api = `${proxy}https://api.weatherapi.com/v1/current.json?key=3ead6e9c90da48d7911201423210106&q=${locaTion.value}&aqi=no`;
-//
-//    fetch(api)
-//        .then(response => {
-//            return response.json();
-//        })
-//        .then(data => {
-//            console.log(data);
-//            if (data.hasOwnProperty('error')) {
-//                cityName.textContent = 'No Matching Location';
-//                tempDesc.textContent = 'invalid location';
-//            } else {
-//
-//                const {
-//                    condition,
-//                    temp_c
-//                } = data.current;
-//
-//                const {
-//                    name,
-//                    country,
-//                    localtime
-//                } = data.location;
-//
-//                weatherIcon.src = condition.icon;
-//                weatherIcon.classList.remove('hidden');
-//
-//                tempDesc.textContent = condition.text;
-//                tempDeg.textContent = temp_c;
-//
-//                cityName.textContent = `${name}, ${country}`;
-//                localTime.textContent = localtime
-//            }
-//        })
-//
-//});
+submit.addEventListener('click', () => {
 
-window.addEventListener('load', () => {
+   const proxy = 'http://cors-anywhere.herokuapp.com/';
+   const api = `${proxy}https://api.weatherapi.com/v1/current.json?key=3ead6e9c90da48d7911201423210106&q=${locaTion.value}&aqi=no`;
 
-    fetch('http://cors-anywhere.herokuapp.com/http://ipinfo.io')
-        .then(response => {
-            console.log(response);
-        })
-    //    let long;
-    //    let lat;
-    //
-    //    if (navigator.geolocation) {
-    //        navigator.geolocation.getCurrentPosition(position => {
-    //
-    //        })
-    //    }
+   fetch(api)
+       .then(response => {
+           return response.json();
+       })
+       .then(data => {
+           console.log(data);
+           if (data.hasOwnProperty('error')) {
+               cityName.textContent = 'No Matching Location';
+               tempDesc.textContent = 'invalid location';
+           } else {
+
+               const {
+                   condition,
+                   temp_c
+               } = data.current;
+
+               const {
+                   name,
+                   country,
+                   localtime
+               } = data.location;
+
+               weatherIcon.src = condition.icon;
+               weatherIcon.classList.remove('hidden');
+
+               tempDesc.textContent = condition.text;
+               tempDeg.textContent = temp_c;
+
+               cityName.textContent = `${name}, ${country}`;
+               localTime.textContent = localtime
+           }
+       })
 
 });
+
+// window.addEventListener('load', () => {
+
+//     fetch('http://cors-anywhere.herokuapp.com/http://ipinfo.io')
+//         .then(response => {
+//             console.log(response);
+//         })
+//     //    let long;
+//     //    let lat;
+//     //
+//     //    if (navigator.geolocation) {
+//     //        navigator.geolocation.getCurrentPosition(position => {
+//     //
+//     //        })
+//     //    }
+
+// });
